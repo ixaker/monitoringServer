6eb5fecc1d216e61260615ac3eb931b243e75e09
xaker <xaker.dnepr@gmail.com>
2024-04-01 14:28:58 +0000

    add SSL

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/monitoring.qpart.com.ua/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/monitoring.qpart.com.ua/fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(5000);
 }
 bootstrap();

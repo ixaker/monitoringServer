d68e887770b238022ead5ee210ed480df5231f50
xaker <xaker.dnepr@gmail.com>
2024-04-03 15:27:23 +0000

    add telegram

--- src/websocket/websocket.service.ts
+++ src/websocket/websocket.service.ts
@@ -1,6 +1,7 @@
 import { OnGatewayConnection, WebSocketGateway, SubscribeMessage, WebSocketServer  } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { Devices } from './../devices/devices';
+import * as TelegramBot from 'node-telegram-bot-api';
 
 @WebSocketGateway({ cors: { origin: '*' }})
 
@@ -8,7 +9,8 @@ export class SocketService implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
   dev = new Devices(this.sendInfoForWebClient.bind(this));
-
+  bot = new TelegramBot('5732114057:AAHK-S0mlws8G4-d9AOwJ0rlqX6ikeI_nSY', { polling: true });
+  chatId = '672754822';
 
   // from Devices
   @SubscribeMessage('info')
@@ -31,6 +33,8 @@ export class SocketService implements OnGatewayConnection {
   @SubscribeMessage('telegram')
   handleMessageTelegram(client: any, data: any): void {
     console.log('SubscribeMessage - telegram', data);
+
+    this.bot.sendMessage(this.chatId, data);
   }
 
   // from Devices 
@@ -41,6 +45,19 @@ export class SocketService implements OnGatewayConnection {
       this.server.emit('webclient', data);
   }
 
+  // from WebClients
+  @SubscribeMessage('disable')
+  handleMessageDisable(client: any, payload: any): void {
+    console.log('SubscribeMessage - disable');
+
+    this.dev.getList().forEach((device) => {
+      if (device.online) {
+        this.server.emit(device.id, {topic: 'command', payload: 'Stop-Computer -Force'});
+      }
+    });
+
+    this.server.emit(payload.id, payload);
+  }
 
   handleConnection(client: any, ...args: any[]) {
     console.log('Client connected', client.id, client.handshake.headers.type || 'no type');

e8ad69ae9b594ecc134bef4f9e9d048451bfc07e
Vitalii Bilostotskyi <forcedarv@gmail.com>
2025-08-15 11:40:20 +0300

    accepts changes

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,5 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
-<<<<<<< HEAD
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(
-      '/etc/letsencrypt/live/monitoring.qpart.com.ua/privkey.pem',
-    ),
-    cert: fs.readFileSync(
-      '/etc/letsencrypt/live/monitoring.qpart.com.ua/fullchain.pem',
-    ),
-  };
-=======
 import { AppModule } from './app.module';
 // import * as fs from 'fs';
 import * as dotenv from 'dotenv';
@@ -30,7 +15,6 @@ async function bootstrap() {
   //     '/etc/letsencrypt/live/monitoring.qpart.com.ua/fullchain.pem',
   //   ),
   // };
->>>>>>> 768e081eadb03f58bcbc70315b8d65e1b41c0f11
 
   const corsOptions: CorsOptions = {
     origin: '*',
